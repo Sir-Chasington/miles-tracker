0a94789638d586f4ce7ebff3ae538ba3cbaf36ed
Chase Lester <chaselester.cl@gmail.com>
2024-06-07 17:27:34 -0600

    updated

--- miles.js
+++ miles.js
@@ -351,25 +351,6 @@ window.onload = function() {
             setTextContent('reportedMiles', reportedMilesObj.currentValue);
         }
     }
-
-    // average section
-    const averageSection = localStorage.getItem('currentSession');
-    if (averageSection) {
-        const parsed = JSON.parse(averageSection);
-        setTextContent('recentMiles', `${parsed.miles} miles`);
-        setTextContent('recentBattery', `${parsed.percent}% used`);
-        setTextContent('recentAverageBattery', `${parsed.averagePerPercent} miles per single percent`);
-        setTextContent('recentFullDistance', `${parsed.potentialTravel} total miles`);
-    }
-
-    const allConsumptionSection = localStorage.getItem('overalSession');
-    if (allConsumptionSection) {
-        const parsed = JSON.parse(allConsumptionSection);
-        setTextContent('allMiles', `${parsed.miles} miles`);
-        setTextContent('allBattery', `${parsed.percent}% used`);
-        setTextContent('allAverageBattery', `${parsed.averagePerPercent} miles per single percent`);
-        setTextContent('allFullDistance', `${parsed.potentialTravel} miles`);
-    }
 };
 
 function setReportedMiles() {
@@ -421,84 +402,6 @@ document.getElementById('actualMilesInput').addEventListener('keydown', function
 
 document.getElementById('submitBtn').addEventListener('click', function() {
     setValuesAndUpdateReportedMiles();
-});
-
-// reset miles section values
-function averageAreaReset() {
-    document.getElementById('milesTraveled').value = '';
-    document.getElementById('percentUsed').value = '';
-    setTextContent('milesError', '');
-}
-
-// Function to calculate average miles traveled with battery consumption
-let milesTraveled;
-let percentUsed;
-function setAverageMilesAndBattery() {
-    if (!milesTraveled || !percentUsed) {
-        setTextContent('milesError', 'Missing miles value or batter used value');
-    } else {
-        const recentAverageMilesPerPercent = milesTraveled/percentUsed;
-        const currentSession = {
-            miles: milesTraveled,
-            percent: percentUsed,
-            averagePerPercent: recentAverageMilesPerPercent,
-            potentialTravel: recentAverageMilesPerPercent * 100
-        }
-        localStorage.setItem('currentSession', JSON.stringify(currentSession));
-        setTextContent('recentMiles', `${milesTraveled} miles`);
-        setTextContent('recentBattery', `${percentUsed}% used`);
-        setTextContent('recentAverageBattery', `${recentAverageMilesPerPercent} miles per single percent`);
-        setTextContent('recentFullDistance', `${currentSession.potentialTravel} miles`);
-
-        // do we want to keep a historical snapshot of all reported miles?
-
-        const overalSession = localStorage.getItem('overalSession'); 
-        if (!overalSession) {
-            localStorage.setItem('overalSession', JSON.stringify(currentSession));
-            setTextContent('allMiles', `${milesTraveled} miles`);
-            setTextContent('allBattery', `${percentUsed}% used`);
-            setTextContent('allAverageBattery', `${recentAverageMilesPerPercent} miles per single percent`);
-            setTextContent('allFullDistance', `${currentSession.potentialTravel} miles`);
-        } else {
-            const overallObj = JSON.parse(overalSession);
-            const totalMilesTraveled = overallObj.miles + milesTraveled;
-            const totalBatteryPercentUsed = overallObj.percent + percentUsed;
-            const totalAverageMilesPerBatteryPercent = totalMilesTraveled/totalBatteryPercentUsed;
-            const newObj = {
-                miles: totalMilesTraveled,
-                percent: totalBatteryPercentUsed,
-                averagePerPercent: totalAverageMilesPerBatteryPercent,
-                potentialTravel: totalAverageMilesPerBatteryPercent * 100
-            }
-
-            localStorage.setItem('overalSession', JSON.stringify(newObj));
-            setTextContent('allMiles', `${newObj.miles} miles`);
-            setTextContent('allBattery', `${newObj.percent}% used`);
-            setTextContent('allAverageBattery', `${newObj.averagePerPercent} miles per single percent`);
-            setTextContent('allFullDistance', `${newObj.potentialTravel} miles`);
-        }
-        averageAreaReset();
-    }
-}
-
-// Event listener for calculating miles traveled and battery percent used
-document.getElementById('milesTraveled').addEventListener('keyup', function(event) {
-    event.preventDefault();
-    milesTraveled = Number(event.target.value);
-
-    if (event.key === 'Enter') {
-        setAverageMilesAndBattery();
-    }
-});
-
-// Event listener for calculating percent of batter with miles traveled
-document.getElementById('percentUsed').addEventListener('keyup', function(event) {
-    event.preventDefault();
-    percentUsed = Number(event.target.value);
-
-    if (event.key === 'Enter') {
-        setAverageMilesAndBattery();
-    }
 });
 
 function updateDifference(newActualMilesUsed) {
